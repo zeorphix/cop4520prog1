50c2c4de1f89357d0bdfa3091fc9c1e43c79b00a
zeorphix <tieue45@gmail.com>
2024-01-25 21:10:09 -0500

    comments

--- assignment1.cpp
+++ assignment1.cpp
@@ -11,19 +11,17 @@
 
 using namespace std;
 
-// - spawn 8 threads that will perform the necessary computation
+const int NUM_THREADS = 8;
+const int MIN = 1;
+const int MAX = 100000000;
+const int SEGMENT_SIZE = (MAX - MIN + 1) / NUM_THREADS;
 
-// - work is distributed such that the computational execution time is
-//   approximately equivalent among the threads
-
-// - output to primes.txt
-//   <execution time> <tot al number of primes found> <sum of all primes found>
-//   <top ten maximum primes, listed in order from lowest to highest>
-
-//  DON'T INCLUDE ZERO OR ONE IN PRIMES FOUND
+int totalPrimes = 0;
+long long sumPrimes = 0;
 
 // Calculates if a given number is prime or not
-bool isPrime(int num) {
+bool isPrime(int num) 
+{
     if (num <= 1)
         return false;
 
@@ -42,22 +40,17 @@ void sieve(int start, int end)
 
 int main(void) 
 {
-    const int NUM_THREADS = 8;
-    const int MIN = 1;
-    const int MAX = 100000000;
-    const int SEGMENT_SIZE = (MAX - MIN + 1) / NUM_THREADS;
-
-    int totalPrimes = 0;
-    long long sumPrimes = 0;
+    std::vector<int> primesList;
 
     std::cout << "Hello World!" << endl;
 
     // Execution start time prior to thread spawn
-    
     auto startTime = std::chrono::high_resolution_clock::now();
     
     std::vector<std::thread> threads;
 
+    // Spawn 8 threads, each sieving a portion of the total range
+	// Each thread has an equivalent workload of range
     for (int i = 0; i < NUM_THREADS; ++i)
     {
         int start = MIN + i * SEGMENT_SIZE;
@@ -68,13 +61,15 @@ int main(void)
     for (auto& t : threads)
         t.join();
     
-    auto endTime = std::chrono::high_resolution_clock::now();
-    
     // Execution end time after all threads complete
+    auto endTime = std::chrono::high_resolution_clock::now();
 
     std::chrono::duration<double, std::milli> executionTime = endTime - startTime;
 
-    // std::thread t1(sieve, num);
+    //  DON'T INCLUDE ZERO OR ONE IN PRIMES FOUND
+    // - output to primes.txt
+    //   <execution time> <tot al number of primes found> <sum of all primes found>
+    //   <top ten maximum primes, listed in order from lowest to highest>
 
     std::cout << executionTime.count() << " ms" << endl;
     
