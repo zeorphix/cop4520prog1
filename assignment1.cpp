e1c52f793fee912155534b6bec74f99cc19a082a
zeorphix <tieue45@gmail.com>
2024-01-25 22:09:41 -0500

    optimize isPrime; more comments
    skips even numbers

--- assignment1.cpp
+++ assignment1.cpp
@@ -6,6 +6,7 @@
 #include <cmath>
 #include <fstream>
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include <vector>
 
@@ -20,10 +21,10 @@ long long sumPrimes = 0;
 // Calculates if a given number is prime or not
 bool isPrime(int num) 
 {
-    if (num <= 1)
+    if (num <= 1 || num > 2 && num % 2 == 0)
         return false;
 
-    for (int i = 2; i <= sqrt(num); ++i)
+    for (int i = 3; i <= sqrt(num); i += 2)
         if (num % i == 0)
             return false;
     
@@ -36,23 +37,30 @@ void sieve(int start, int end)
     using namespace std;
 
     cout << start << " + " << end << endl;
+
+    cout << "is " << start << " prime? " << isPrime(start) << endl;
+    cout << "is " << end << " prime? " << isPrime(end) << endl;
 }
 
 int main(void) 
 {
+    // Let's make it easier to type.
     using namespace std;
 
-    vector<int> listPrimes;
+    // Initialize a 2D integer vector of length
+    // equivalent to the number of threads.
+    // Each thread has their own list of primes.
+    vector<vector<int>> listPrimes(NUM_THREADS);
 
     cout << "Hello World!" << endl;
 
-    // Execution start time prior to thread spawn
+    // Execution start time prior to thread spawn.
     auto startTime = chrono::high_resolution_clock::now();
     
     vector<thread> threads;
 
-    // Spawn 8 threads, each sieving a portion of the total range
-	// Each thread has an equivalent workload of range
+    // Spawn 8 threads, each sieving a portion of the total range.
+	// Each thread has an equivalent workload of range.
     for (int i = 0; i < NUM_THREADS; ++i)
     {
         int start = MIN + i * SEGMENT_SIZE;
@@ -63,7 +71,7 @@ int main(void)
     for (auto& t : threads)
         t.join();
     
-    // Execution end time after all threads complete
+    // Execution end time after all threads complete.
     auto endTime = chrono::high_resolution_clock::now();
 
     chrono::duration<double, milli> executionTime = endTime - startTime;
